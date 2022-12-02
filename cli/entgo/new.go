6756b1de78dc0d64876e6b66e2fd5e399002f0c4
Adnaan Badr <badr.adnaan@gmail.com>
2022-12-02 14:47:13 +0100

    cleanup old cli gen

--- cli/entgo/new.go
+++ cli/entgo/new.go
@@ -1,12 +1,19 @@
 package entgo
 
 import (
+	"bytes"
 	"embed"
+	"go/format"
 	"io/fs"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
+	"text/template"
+
+	"github.com/Masterminds/sprig"
+	"github.com/yosssi/gohtml"
 )
 
 var simpleAssetsPath = "simple_assets"
@@ -34,4 +41,37 @@ func New(projectName string) {
 		return nil
 	})
 
+}
+
+func execTextTemplate(inFile, outFile string, vars map[string]any) {
+	inFileData, err := simpleAssets.ReadFile(inFile)
+	checkerr(err)
+
+	t := template.Must(
+		template.New("gen").
+			Delims("[[", "]]").
+			Funcs(template.FuncMap(sprig.FuncMap())).
+			Parse(string(inFileData)))
+	buf := new(bytes.Buffer)
+	err = t.ExecuteTemplate(buf, "gen", vars)
+	checkerr(err)
+
+	if filepath.Ext(outFile) == ".go" {
+		inFileData, err = format.Source(buf.Bytes())
+		checkerr(err)
+	} else if filepath.Ext(outFile) == ".html" {
+		inFileData = gohtml.FormatBytes(buf.Bytes())
+	} else {
+		inFileData = buf.Bytes()
+	}
+
+	err = ioutil.WriteFile(outFile, inFileData, 0644)
+	checkerr(err)
+	buf.Reset()
+}
+
+func checkerr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
