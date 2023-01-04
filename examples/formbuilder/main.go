b72bcb65525d0cf0b7385f515ca82e862498bd91
Adnaan Badr <badr.adnaan@gmail.com>
2023-01-05 00:01:04 +0100

    initial example

---
+++ examples/formbuilder/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/livefir/fir"
+)
+
+func index() fir.RouteOptions {
+	return fir.RouteOptions{
+		fir.ID("quickstart"),
+		fir.Content("app.html"),
+		fir.OnLoad(func(ctx fir.RouteContext) error {
+			return nil
+		}),
+		fir.OnEvent("add", func(ctx fir.RouteContext) error {
+			return nil
+		}),
+		fir.OnEvent("remove", func(ctx fir.RouteContext) error {
+			return nil
+		}),
+	}
+}
+
+func main() {
+	controller := fir.NewController("quickstart_app", fir.DevelopmentMode(true))
+	http.Handle("/", controller.RouteFunc(index))
+	http.ListenAndServe(":9867", nil)
+}
