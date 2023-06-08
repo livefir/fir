aaf376c223a4c5a5bc9168856c1a3499f528d8a3
Adnaan Badr <badr.adnaan@gmail.com>
2023-06-08 10:05:04 +0200

    use websocket only for server side push

--- alpinejs-plugin/src/index.js
+++ alpinejs-plugin/src/index.js
@@ -532,34 +532,31 @@ const Plugin = (Alpine) => {
             )
         }
 
-        if (socket && socket.emit(firEvent)) {
-        } else {
-            const body = JSON.stringify(firEvent)
-            fetch(window.location.pathname, {
-                method: 'POST',
+        const body = JSON.stringify(firEvent)
+        fetch(window.location.pathname, {
+            method: 'POST',
 
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-FIR-MODE': 'event',
-                },
-                body: body,
+            headers: {
+                'Content-Type': 'application/json',
+                'X-FIR-MODE': 'event',
+            },
+            body: body,
+        })
+            .then((response) => {
+                if (response.redirected) {
+                    window.location.href = response.url
+                }
+                return response.json()
             })
-                .then((response) => {
-                    if (response.redirected) {
-                        window.location.href = response.url
-                    }
-                    return response.json()
-                })
-                .then((serverEvents) => {
-                    dispatchServerEvents(serverEvents)
-                })
-                .catch((error) => {
-                    console.error(
-                        `${eventIdLower} error: ${error}, request body: ${body}`,
-                        error
-                    )
-                })
-        }
+            .then((serverEvents) => {
+                dispatchServerEvents(serverEvents)
+            })
+            .catch((error) => {
+                console.error(
+                    `${eventIdLower} error: ${error}, request body: ${body}`,
+                    error
+                )
+            })
     }
 
     Alpine.plugin(morph)
