8e2f6ac5182a3bbbe02786981072415e7a13b107
Adnaan <badr.adnaan@gmail.com>
2025-07-28 23:56:52 +0200

    remove empty file

--- file.go
+++
@@ -1,4 +0,0 @@
-package fir
-
-// This file provides backward compatibility by importing the internal file package.
-// All file functionality has been moved to internal/file for better organization.
